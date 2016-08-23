168a21fd9a4be0543601f72e3a02aee8a988cc10
Sebastian Stark <sstark@mailbox.org>
2016-08-23 14:09:01 +0200

    rename FX* to fx_* for better readabity and conformance

--- src/midi.go
+++ src/midi.go
@@ -11,10 +11,10 @@ type event struct {
 	Velocities row
 }
 
-func FXrandv(part part) int {
+func fx_randv(part part) int {
 	for _, ef := range part.Fx {
 		if v, ok := ef["randv"]; ok {
-			debugf("FXrandv(): found randv value %v", v)
+			debugf("fx_randv(): found randv value %v", v)
 			randomness, err := strconv.Atoi(v)
 			if err == nil {
 				return randomness
@@ -24,15 +24,15 @@ func FXrandv(part part) int {
 	return 0
 }
 
-func FXrampv(part part) row {
+func fx_rampv(part part) row {
 	vmap := make(row, len(part.Lanes[0]))
 	if len(vmap) == 0 {
-		debugf("FXrampv(): empty part: %v", part)
+		debugf("fx_rampv(): empty part: %v", part)
 		return nil
 	}
 	for _, ef := range part.Fx {
 		if v, ok := ef["rampv"]; ok {
-			debugf("FXrampv(): found rampv value %v", v)
+			debugf("fx_rampv(): found rampv value %v", v)
 			values := strings.Split(v, "-")
 			if len(values) != 2 {
 				return nil
@@ -59,14 +59,14 @@ func genVelocityMap(part part) matrix {
 	// if available, use ramp as base for vmap
 	// otherwise initialise one with max velocity
 	var vmatrix matrix
-	vmap := FXrampv(part)
+	vmap := fx_rampv(part)
 	if vmap == nil {
 		vmap = make(row, len(part.Lanes[0]))
 		for i := range vmap {
 			vmap[i] = 127
 		}
 	}
-	randomness := FXrandv(part)
+	randomness := fx_randv(part)
 	for i := range vmap {
 		if randomness != 0 {
 			v := (rand.Int() % randomness) - (randomness / 2)
