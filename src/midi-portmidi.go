08418f996e73f0dace9e17220ccd7084e0ee09fb
Sebastian Stark <sstark@mailbox.org>
2016-08-26 00:42:12 +0200

    print found portmidi outputs on startup

--- src/midi-portmidi.go
+++ src/midi-portmidi.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/rakyll/portmidi"
 )
 
@@ -14,7 +15,22 @@ func sendMidiNote(channel, note, velocity int) {
 
 func initMidi() {
 	err := portmidi.Initialize()
+	midiDevCount := portmidi.CountDevices() // returns the number of MIDI devices
 	defaultOut := portmidi.DefaultOutputDeviceID()
+	var midiDeviceInfo *portmidi.DeviceInfo
+	fmt.Println("MIDI outputs found:")
+	for i := 0; i < midiDevCount; i++ {
+		midiDeviceInfo = portmidi.Info(portmidi.DeviceID(i)) // returns info about a MIDI device
+		fmt.Printf("%d: ", i)
+		if midiDeviceInfo.IsOutputAvailable {
+			fmt.Print(midiDeviceInfo.Interface, "/", midiDeviceInfo.Name)
+		}
+		if i == int(defaultOut) {
+			fmt.Println(" (default)")
+		} else {
+			fmt.Println()
+		}
+	}
 	midiOut, err = portmidi.NewOutputStream(defaultOut, 1024, 0)
 	checkErr(err)
 }
