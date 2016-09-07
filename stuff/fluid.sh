06d673ad50196815f2745e83de2fbd82ae9cc121
Sebastian Stark <sstark@mailbox.org>
2016-09-07 23:18:34 +0200

    allow arguments to be passed to fluidsynth

--- stuff/fluid.sh
+++ stuff/fluid.sh
@@ -5,9 +5,10 @@ print "\
 prog 5 47
 prog 6 113
 prog 7 13
+prog 10 11
 gain 3
 reverb off
 channels
 " >$inputfile
-fluidsynth -a coreaudio -m coremidi ~/Music/FluidR3\ GM2-2.SF2 -f $inputfile
+fluidsynth -a coreaudio -m coremidi "$@" ~/Music/FluidR3\ GM2-2.SF2 -f $inputfile
 rm -f $inputfile
